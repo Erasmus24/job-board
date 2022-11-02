8694814103c6b4d9413c4b05b2764105c3adcdcf
sphiwe <sphiwe@thedigitalacademy.co.za>
2022-11-02 13:47:32 +0200

    Create the job detail page

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Jobs from 'components/Jobs'
+import prisma from 'lib/prisma'
+import { getJobs } from 'lib/data.js'
+
+export default function Home({ jobs }) {
+  return (
+    <div className='mt-10'>
+      <div className='text-center p-4 m-4'>
+        <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
+      </div>
+      {jobs.map((job, index) => (
+        <p>{job.title}</p>
+      ))}
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  let jobs = await getJobs(prisma)
+	jobs = JSON.parse(JSON.stringify(jobs))
+
+  return {
+    props: {
+      jobs,
+    },
+  }
+}
