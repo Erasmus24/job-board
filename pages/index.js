4643078a06bf4a86a76628738f13dc764a38b6f7
sphiwe <sphiwe@thedigitalacademy.co.za>
2022-11-03 07:45:01 +0200

    Let applicants login and setup their profile (choose if company, choose name, add profile info)

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,35 @@
-import Jobs from 'components/Jobs'
-import prisma from 'lib/prisma'
-import { getJobs } from 'lib/data.js'
+import Jobs from "components/Jobs";
+import prisma from "lib/prisma";
+import { getJobs } from "lib/data.js";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Home({ jobs }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (session && !session.user.name) {
+    router.push("/setup");
+  }
   return (
-    <div className='mt-10'>
-      <div className='text-center p-4 m-4'>
-        <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
+    <div className="mt-10">
+      <div className="text-center p-4 m-4">
+        <h2 className="mb-10 text-4xl font-bold">Find a job!</h2>
       </div>
       {jobs.map((job, index) => (
         <p>{job.title}</p>
       ))}
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
-  let jobs = await getJobs(prisma)
-	jobs = JSON.parse(JSON.stringify(jobs))
+  let jobs = await getJobs(prisma);
+  jobs = JSON.parse(JSON.stringify(jobs));
 
   return {
     props: {
       jobs,
     },
-  }
+  };
 }
