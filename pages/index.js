1fc4cc75d12cfd3b2a1e68565084e2ce4a78f68e
sphiwe <sphiwe@thedigitalacademy.co.za>
2022-11-03 08:31:46 +0200

    Let applicants login and setup their profile (choose if company, choose name, add profile info)

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Jobs from "components/Jobs";
 import prisma from "lib/prisma";
-import { getJobs } from "lib/data.js";
-import { useSession } from "next-auth/react";
+import { getJobs, getUser } from 'lib/data.js'
+import { useSession, getSession } from 'next-auth/react'
 import { useRouter } from "next/router";
 
-export default function Home({ jobs }) {
+
+export default function Home({ jobs, user }) {
   const { data: session, status } = useSession();
   const router = useRouter();
 
@@ -16,20 +17,67 @@ export default function Home({ jobs }) {
       <div className="text-center p-4 m-4">
         <h2 className="mb-10 text-4xl font-bold">Find a job!</h2>
       </div>
-      {jobs.map((job, index) => (
-        <p>{job.title}</p>
-      ))}
+      {session && (
+  <>
+    <p className='mb-10 text-2xl font-normal'>
+      Welcome, {user.name}
+      {user.company && (
+        <span className='bg-black text-white uppercase text-sm p-2'>
+          Company
+        </span>
+      )}
+    </p>
+    {user.company ? (
+      <>
+        <button
+          className='border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+        >
+          click here to post a new job
+        </button>
+        <button
+          className='ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+        >
+          see all the jobs you posted
+        </button>
+      </>
+    ) : (
+      <>
+        <button
+          className='ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+        >
+          see all the jobs you applied to
+        </button>
+      </>
+    )}
+  </>
+)}
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  let jobs = await getJobs(prisma);
-  jobs = JSON.parse(JSON.stringify(jobs));
+	const session = await getSession(context)
+
+  let jobs = await getJobs(prisma)
+  jobs = JSON.parse(JSON.stringify(jobs))
+
+	if (!session) {
+    return {
+      props: { jobs },
+    }
+  }
+  if (!session) {
+    return {
+      props: { jobs },
+    }
+  }
+  let user = await getUser(session.user.id, prisma)
+  user = JSON.parse(JSON.stringify(user))
 
   return {
     props: {
       jobs,
+      user,
     },
-  };
+  }
 }
